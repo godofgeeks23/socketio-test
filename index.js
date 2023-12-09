16122102d166fce8f01bffd28876c0bd1bd65bf8
Aviral Srivastava <72144346+godofgeeks23@users.noreply.github.com>
2023-12-09 23:31:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ const app = express();
 const server = createServer(app);
 
 app.get('/', (req, res) => {
-    res.sendFile(join(__dirname, 'index.html'));
-  });
-  
+  res.sendFile(join(__dirname, 'index.html'));
+});
+
 
 server.listen(3000, () => {
   console.log('server running at http://localhost:3000');
