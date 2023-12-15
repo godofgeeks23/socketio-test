bdff1802724467436ac39ec443d4a7e514bceba5
Aviral Srivastava <72144346+godofgeeks23@users.noreply.github.com>
2023-12-15 22:46:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ const express = require('express');
 const { createServer } = require('node:http');
 const { join } = require('node:path');
 
+// create express app
 const app = express();
+// create http server using express app
 const server = createServer(app);
 
+// serve static files from the public directory
 app.get('/', (req, res) => {
   res.sendFile(join(__dirname, 'index.html'));
 });
